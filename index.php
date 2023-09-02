1d046b5300f7d1c4887fbf56fe1b65a4a669bab2
kiwibo2120 <143869424+kiwibo2120@users.noreply.github.com>
2023-09-02 14:30:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+echo "# ujipdt" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kiwibo2120/ujipdt.git
+git push -u origin main
